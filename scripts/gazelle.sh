4abadef540e5c5155aa0e6cc7b19221cbaa4b82d
Leopold Schabel <leo@nexantic.com>
2019-10-24 18:19:53 +0200

    Specify -prune=true Gazelle flag
    This cleans up our repositories.bzl file.
    
    Update BUILD.bazel files and go.mod. `google.golang.org/appengine v1.4.0`
    disappears due to a bug that was fixed in Go 1.13 - make sure your
    workstation is up to date, an automated installer for Fedora was
    added in rWdfcd0da736.
    
    Test Plan: Ran scripts/gazelle.sh, repositories.bzl was updated properly.
    
    X-Origin-Diff: phab/D216
    GitOrigin-RevId: 2435f88beb610845edfc8a6c50e4ef16edb895ea

--- scripts/gazelle.sh
+++ scripts/gazelle.sh
@@ -2,4 +2,4 @@
 # gazelle.sh regenerates BUILD.bazel files for Go source files.
 
 bazel run //:gazelle -- update
-bazel run //:gazelle -- update-repos -from_file=go.mod -to_macro=repositories.bzl%go_repositories
+bazel run //:gazelle -- update-repos -from_file=go.mod -to_macro=repositories.bzl%go_repositories -prune=true
