e47ace84cb3e30375dcb4236c17ee9710a77a6ad
Lorenz Brun <lorenz@brun.one>
2019-11-06 12:45:47 +0100

    Fixes bazel_copy_generated_for_ide paths
    This fixes the paths in bazel_copy_generated
    for the new monorepo layout
    
    Test Plan: By using it :)
    
    X-Origin-Diff: phab/D246
    GitOrigin-RevId: 01fc0488ef4e2f6412ae458b4f2010a975a83692

--- scripts/bazel_copy_generated_for_ide.sh
+++ scripts/bazel_copy_generated_for_ide.sh
@@ -2,5 +2,5 @@
 # Copy generated Go protobuf libraries to a place where a non-Bazel-aware IDE can find them.
 # Locally, a symlink will be sufficient.
 
-mkdir -p smalltown/generated
-rsync -av --delete --exclude '*.a' bazel-bin/smalltown/api/*/linux_amd64_stripped/*/git.monogon.dev/source/nexantic.git/smalltown/generated/* smalltown/generated/
+mkdir -p core/generated
+rsync -av --delete --exclude '*.a' bazel-bin/core/api/*/linux_amd64_stripped/*/git.monogon.dev/source/nexantic.git/core/generated/* core/generated/
