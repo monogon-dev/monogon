3cb0f4bdc0766574854a2db578fddd97b6648f6e
Leopold Schabel <leo@nexantic.com>
2021-07-19 15:22:07 +0200

    scripts: check if running from original checkout
    Change-Id: I42e6b9e38a86a05c8ddbd2716ed2bd4d1db59331
    Reviewed-on: https://review.monogon.dev/c/monogon/+/217
    Reviewed-by: Sergiusz Bazanski <serge@nexantic.com>

--- scripts/run_in_container.sh
+++ scripts/run_in_container.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [[ "$(podman inspect monogon-dev --format "{{ (index .Mounts 0).Source }}")" != "$(pwd)" ]]; then
+    echo "Please run this wrapper from the original checkout"
+    exit 1
+fi
+
 podman exec -it monogon-dev $@
