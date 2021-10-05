27b6c4fd36a3e664cb9ed209e498404090d550a2
Serge Bazanski <serge@monogon.tech>
2021-10-05 18:55:46 +0200

    m/n/c/curator: use UnimplementedXXX stubs for follower unimplemented code
    Change-Id: Iecf9adf91c8ae4c6af5854e35dbf3362b1b31865
    Reviewed-on: https://review.monogon.dev/c/monogon/+/344
    Reviewed-by: Lorenz Brun <lorenz@monogon.tech>

--- metropolis/node/core/curator/impl_follower.go
+++ metropolis/node/core/curator/impl_follower.go
@@ -1,30 +1,12 @@
 package curator
 
 import (
-	"context"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
 	cpb "source.monogon.dev/metropolis/node/core/curator/proto/api"
 	apb "source.monogon.dev/metropolis/proto/api"
 )
 
 type curatorFollower struct {
-}
-
-func (f *curatorFollower) Watch(req *cpb.WatchRequest, srv cpb.Curator_WatchServer) error {
-	return status.Error(codes.Unimplemented, "curator follower not implemented")
-}
-
-func (f *curatorFollower) Escrow(srv apb.AAA_EscrowServer) error {
-	return status.Error(codes.Unimplemented, "curator follower not implemented")
-}
-
-func (f *curatorFollower) GetRegisterTicket(_ context.Context, _ *apb.GetRegisterTicketRequest) (*apb.GetRegisterTicketResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "curator follower not implemented")
-}
-
-func (f *curatorFollower) UpdateNodeStatus(ctx context.Context, req *cpb.UpdateNodeStatusRequest) (*cpb.UpdateNodeStatusResponse, error) {
-	return nil, status.Error(codes.Unimplemented, "curator follower not implemented")
+	apb.UnimplementedAAAServer
+	apb.UnimplementedManagementServer
+	cpb.UnimplementedCuratorServer
 }
