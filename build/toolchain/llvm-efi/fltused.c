9ee160e87a38b185af57de2b9188cc19d53cd1d1
Lorenz Brun <lorenz@monogon.tech>
2023-08-29 23:07:51 +0200

    b/t/llvm-efi: make fltused symbol weak
    Rust's libcore for UEFI ships this hack as well, causing a duplicate
    symbol error when linking. Marking this weak is also enough for clang to
    keep this for linking, so the separate used attribute is not needed.
    
    Change-Id: I1f6aad626b235bbe1e613c5427b8c4fbd0dc330e
    Reviewed-on: https://review.monogon.dev/c/monogon/+/2102
    Tested-by: Jenkins CI
    Reviewed-by: Serge Bazanski <serge@monogon.tech>

--- build/toolchain/llvm-efi/fltused.c
+++ build/toolchain/llvm-efi/fltused.c
@@ -3,4 +3,4 @@
 // it we can just define it as zero.
 // See https://github.com/rust-lang/rust/issues/62785#issuecomment-531186089 for more discussion.
 // Since building static libraries is not possible with Bazel this is compiled and checked in.
-int _fltused __attribute__((used)) = 0;
+int _fltused __attribute__((weak)) = 0;
