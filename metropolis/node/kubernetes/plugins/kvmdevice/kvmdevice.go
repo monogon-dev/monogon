monogon-dev/monogon

commit d5f851bb477638436826adec756fe562db526865

treewide: replace error comparisons and assertions with errors.Is
Change-Id: Id2424eb155f2c6842c72c5fafd124d428ef901f2
Reviewed-on: https://review.monogon.dev/c/monogon/+/2994
Tested-by: Jenkins CI
Reviewed-by: Serge Bazanski <serge@monogon.tech>

metropolis/node/kubernetes/plugins/kvmdevice/kvmdevice.go
@@ -26,6 +26,7 @@ package kvmdevice
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -158,7 +159,7 @@ func (k *Plugin) Run(ctx context.Context) error {
 	kvmDevNode, err := deviceNumberFromString(string(kvmDevRaw))
 
 	err = unix.Mknod("/dev/kvm", 0660, int(kvmDevNode))
-	if err != nil && err != unix.EEXIST {
+	if err != nil && errors.Is(err, unix.EEXIST) {
 		return fmt.Errorf("failed to create KVM device node: %v", err)
 	}
 

cloud/apigw/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"net"
 	"net/http"
@@ -37,9 +38,9 @@ func (c *Config) RegisterFlags() {
 }
 
 // Server runs the apigw server. It listens on two interfaces:
-//  - Internal gRPC, which is authenticated using TLS and authorized by CA. This
-//    is to be used for internal RPCs, eg. management/debug.
-//  - Public gRPC-Web, which is currently unauthenticated.
+//   - Internal gRPC, which is authenticated using TLS and authorized by CA. This
+//     is to be used for internal RPCs, eg. management/debug.
+//   - Public gRPC-Web, which is currently unauthenticated.
 type Server struct {
 	Config Config
 
@@ -66,7 +67,7 @@ func (s *Server) startInternalGRPC(ctx context.Context) {
 	klog.Infof("Internal gRPC listening on %s", s.ListenGRPC)
 	go func() {
 		err := g.Serve(lis)
-		if err != ctx.Err() {
+		if !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Internal gRPC serve failed: %v", err)
 		}
 	}()
@@ -91,7 +92,7 @@ func (s *Server) startPublic(ctx context.Context) {
 	klog.Infof("Public API listening on %s", s.ListenPublic)
 	go func() {
 		err := server.Serve(lis)
-		if err != ctx.Err() {
+		if !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Public API serve failed: %v", err)
 		}
 	}()

cloud/bmaas/scruffy/server.go
@@ -9,6 +9,7 @@ package scruffy
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -77,7 +78,7 @@ func (s *Server) Start(ctx context.Context) {
 	go hwr.run(ctx)
 
 	go func() {
-		if err := s.Config.Webug.Start(ctx, conn); err != nil && err != ctx.Err() {
+		if err := s.Config.Webug.Start(ctx, conn); err != nil && !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Failed to start webug: %v", err)
 		}
 	}()

cloud/bmaas/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"net"
@@ -119,7 +120,7 @@ func (s *Server) startPublic(ctx context.Context) {
 	klog.Infof("Public API listening on %s", s.ListenPublic)
 	go func() {
 		err := g.Serve(lis)
-		if err != ctx.Err() {
+		if !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Public gRPC serve failed: %v", err)
 		}
 	}()
@@ -137,7 +138,7 @@ func (s *Server) startInternalGRPC(ctx context.Context) {
 	klog.Infof("Internal gRPC listening on %s", s.ListenGRPC)
 	go func() {
 		err := g.Serve(lis)
-		if err != ctx.Err() {
+		if !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Internal gRPC serve failed: %v", err)
 		}
 	}()
@@ -163,7 +164,7 @@ func (s *Server) Start(ctx context.Context) {
 	s.startInternalGRPC(ctx)
 	s.startPublic(ctx)
 	go func() {
-		if err := s.Config.Webug.Start(ctx, conn); err != nil && err != ctx.Err() {
+		if err := s.Config.Webug.Start(ctx, conn); err != nil && !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Failed to start webug: %v", err)
 		}
 	}()

cloud/shepherd/mini/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -182,7 +183,7 @@ func main() {
 		}
 	}()
 	go func() {
-		if err := c.WebugConfig.Start(ctx, conn); err != nil && err != ctx.Err() {
+		if err := c.WebugConfig.Start(ctx, conn); err != nil && !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Failed to start webug: %v", err)
 		}
 	}()

cloud/shepherd/provider/equinix/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -146,7 +147,7 @@ func main() {
 		}
 	}()
 	go func() {
-		if err := c.WebugConfig.Start(ctx, conn); err != nil && err != ctx.Err() {
+		if err := c.WebugConfig.Start(ctx, conn); err != nil && !errors.Is(err, ctx.Err()) {
 			klog.Exitf("Failed to start webug: %v", err)
 		}
 	}()

cloud/shepherd/provider/equinix/provider.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/netip"
 	"slices"
@@ -342,13 +343,13 @@ func (ep *equinixProvider) sshEquinixUpload(ctx context.Context) error {
 // generated as needed The key is generated as needed
 func (ep *equinixProvider) SSHEquinixEnsure(ctx context.Context) error {
 	k, err := ep.sshEquinix(ctx)
-	switch err {
-	case NoSuchKey:
+	switch {
+	case errors.Is(err, NoSuchKey):
 		if err := ep.sshEquinixUpload(ctx); err != nil {
 			return fmt.Errorf("while uploading key: %w", err)
 		}
 		return nil
-	case nil:
+	case err == nil:
 		if err := ep.sshEquinixUpdate(ctx, k.ID); err != nil {
 			return fmt.Errorf("while updating key: %w", err)
 		}

go/algorithm/toposort/toposort_test.go
@@ -1,6 +1,9 @@
 package toposort
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestBasic(t *testing.T) {
 	// Test the example from Wikipedia's Topological sorting page
@@ -83,7 +86,7 @@ func FuzzTopoSort(f *testing.F) {
 			g.AddEdge(int(a[i]), int(a[i+1]))
 		}
 		solution, err := g.TopologicalOrder()
-		if err == ErrCycle {
+		if errors.Is(err, ErrCycle) {
 			// Cycle found
 			return
 		}

commit 2d83a128f6096b8133af9edec00e1cd0cd8215b0

m/n/k/p/kvmdevice: fix device inode error handling
This was broken in d5f851bb47, where the inverted logic was not kept
when migrating to errors.Is.

Change-Id: Id1bbc96f80b33df539a3a5051d56e126bb453390
Reviewed-on: https://review.monogon.dev/c/monogon/+/3077
Tested-by: Jenkins CI
Reviewed-by: Tim Windelschmidt <tim@monogon.tech>

metropolis/node/kubernetes/plugins/kvmdevice/kvmdevice.go
@@ -163,7 +163,7 @@ func (k *Plugin) Run(ctx context.Context) error {
 	}
 
 	err = unix.Mknod("/dev/kvm", 0660, int(kvmDevNode))
-	if err != nil && errors.Is(err, unix.EEXIST) {
+	if err != nil && !errors.Is(err, unix.EEXIST) {
 		return fmt.Errorf("failed to create KVM device node: %v", err)
 	}
 