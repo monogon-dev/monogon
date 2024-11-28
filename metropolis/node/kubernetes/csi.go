ff7452b586134e18af9f1362d7b96dcb64aa8d71
Jan Schär <jan@monogon.tech>
2024-11-28 13:08:55 +0100

    m/node/kubernetes: mount PVs with noexec on the host
    Now that runc always replaces per-mount-point flags when bind-mounting
    volumes inside the container, we can mount them with noexec on the host
    without affecting workloads. This has some security advantages, as any
    executables in volumes are no longer executable from the host.
    
    Change-Id: Id5a8ea8caf702fca58d300fc9e17c21e94ebaf13
    Reviewed-on: https://review.monogon.dev/c/monogon/+/3660
    Reviewed-by: Lorenz Brun <lorenz@monogon.tech>
    Tested-by: Jenkins CI

--- metropolis/node/kubernetes/csi.go
+++ metropolis/node/kubernetes/csi.go
@@ -115,7 +115,7 @@ func (s *csiPluginServer) NodePublishVolume(ctx context.Context, req *csi.NodePu
 			return nil, status.Errorf(codes.Unavailable, "failed to bind-mount volume: %v", err)
 		}
 
-		var flags uintptr = unix.MS_REMOUNT | unix.MS_BIND
+		var flags uintptr = unix.MS_REMOUNT | unix.MS_BIND | unix.MS_NOEXEC | unix.MS_NOSUID | unix.MS_NODEV
 		if req.Readonly {
 			flags |= unix.MS_RDONLY
 		}
