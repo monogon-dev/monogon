3eb7d033642ebaaddccde287d5ad3c8c59f7a576
Tim Windelschmidt <tim@monogon.tech>
2025-01-28 15:21:08 +0100

    osbase/bringup/test: fix flake in bringup test
    Change-Id: I17f51ea922be95e2d81b972b406bee13de3d202c
    Reviewed-on: https://review.monogon.dev/c/monogon/+/3813
    Tested-by: Jenkins CI
    Reviewed-by: Lorenz Brun <lorenz@monogon.tech>

--- osbase/bringup/test/main_succeeded.go
+++ osbase/bringup/test/main_succeeded.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"golang.org/x/sys/unix"
 
@@ -12,6 +13,7 @@ import (
 func main() {
 	bringup.Runnable(func(ctx context.Context) error {
 		fmt.Println("_BRINGUP_LAUNCH_SUCCESS_")
+		time.Sleep(5 * time.Second)
 		unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF)
 		return nil
 	}).Run()
