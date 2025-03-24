173c4124be37a158c7a8e4cafdab1696d05b6e69
Tim Windelschmidt <tim@monogon.tech>
2025-03-24 15:34:34 +0100

    build/toolbase: delete unused code
    Change-Id: I25ac3e6671bb6213aa9563ee9aa2b0afc2337805
    Reviewed-on: https://review.monogon.dev/c/monogon/+/4030
    Tested-by: Jenkins CI
    Reviewed-by: Leopold Schabel <leo@monogon.tech>

--- build/toolbase/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright The Monogon Project Authors.
-// SPDX-License-Identifier: Apache-2.0
-
-// toolbase implements common functionality for tools operating on a Monogon
-// workspace, notably CI and developer tools.
-package toolbase
